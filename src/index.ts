bea0e34f06cc255e30ed5ab4c2725711bbd4967a
emadurandal <emadurandal@gmail.com>
2019-09-21 10:46:34 +0900

    Fix letter case of import statement

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Context from "./Context.js";
 import Material from "./Material.js";
 import Mesh from "./Mesh.js";
-import Gltf2Importer from "./glTF2Importer.js";
+import Gltf2Importer from "./Gltf2Importer.js";
 
 const Spinel = {
   Context,
