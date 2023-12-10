07f90e460ba230bef887007caf812d05416a406d
Yuki Shimada <emadurandal@gmail.com>
2023-12-10 19:47:27 +0900

    refactor: import & export formations

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,2 @@
-import { Context } from "./Context.js";
-import { Material } from "./Material.js";
-import { Primitive } from "./Primitive.js";
-import { Gltf2Importer } from "./Gltf2Importer.js";
-
-const Spinel = {
-  Context,
-  Material,
-  Primitive,
-  Gltf2Importer,
-};
-
+import * as Spinel from './index2.js';
 export default Spinel;
