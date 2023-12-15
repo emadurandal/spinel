5b5d08e86b4332c43445f2e2a6123024daa394ab
Yuki Shimada <emadurandal@gmail.com>
2023-12-15 15:57:58 +0900

    refactor: add git submodule "glTF-Sample-Models"

--- samples/main.ts
+++ samples/main.ts
@@ -4,7 +4,7 @@ async function main() {
 
   const canvas = document.getElementById('world') as HTMLCanvasElement;
   const context = new Spinel.Context(canvas);
-  const entities = await Spinel.Gltf2Importer.import('../assets/gltf/BoxAnimated/glTF/BoxAnimated.gltf', context);
+  const entities = await Spinel.Gltf2Importer.import('../assets/gltf/glTF-Sample-Models/2.0/BoxAnimated/glTF/BoxAnimated.gltf', context);
 
   const gl = context.gl;
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
