27840901d51f7ac46979255768798ff27bbdf12d
Yuki Shimada <emadurandal@gmail.com>
2023-12-16 01:37:17 +0900

    add simple sample

--- samples/gltf/main.ts
+++ samples/gltf/main.ts
@@ -1,10 +1,10 @@
-import Spinel from '../dist/index.js'
+import Spinel from '../../dist/index.js'
 
 async function main() {
 
   const canvas = document.getElementById('world') as HTMLCanvasElement;
   const context = new Spinel.Context(canvas);
-  const entities = await Spinel.Gltf2Importer.import('../assets/gltf/glTF-Sample-Models/2.0/Buggy/glTF/Buggy.gltf', context);
+  const entities = await Spinel.Gltf2Importer.import('../../assets/gltf/glTF-Sample-Models/2.0/Buggy/glTF/Buggy.gltf', context);
 
   const gl = context.gl;
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -17,9 +17,9 @@ async function main() {
 
   const draw = () => {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-  for (const meshEntity of meshEntities) {
-    meshEntity.getMesh()!.draw();
-  }
+    for (const meshEntity of meshEntities) {
+      meshEntity.getMesh()!.draw();
+    }
     requestAnimationFrame(draw);
   };
   draw();
---
+++ samples/simple/main.ts
@@ -0,0 +1,50 @@
+import Spinel from '../../dist/index.js'
+
+async function main() {
+
+  const canvas = document.getElementById('world') as HTMLCanvasElement;
+  const context = new Spinel.Context(canvas);
+
+  const gl = context.gl;
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.enable(gl.DEPTH_TEST);
+
+  const meshEntity = Spinel.Entity.create();
+  const material = new Spinel.Material(context);
+  const vertexData = {
+    position: new Float32Array([
+      -1, -1, 0,
+      1, -1, 0,
+      0, 1, 0
+    ]),
+    // color: [
+    //   1, 0, 0, 1,
+    //   0, 1, 0, 1,
+    //   0, 0, 1, 1
+    // ],
+    indices: new Uint16Array([
+      0, 1, 2
+    ]),
+    mode: Spinel.PrimitiveMode.Triangles,
+  }
+  const primitive = new Spinel.Primitive(material, context, vertexData);
+  const mesh = new Spinel.Mesh([primitive]);
+  meshEntity.addMesh(mesh);
+
+  const cameraEntity = Spinel.Entity.create();
+  cameraEntity.getTransform().setLocalPosition(new Spinel.Vector3(0, 0, 4));
+  cameraEntity.addCamera(Spinel.CameraType.Perspective);
+
+  const meshEntities = Spinel.Entity.getAllMeshEntities();
+
+  const draw = () => {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    for (const meshEntity of meshEntities) {
+      meshEntity.getMesh()!.draw();
+    }
+    requestAnimationFrame(draw);
+  };
+  draw();
+}
+
+main();
